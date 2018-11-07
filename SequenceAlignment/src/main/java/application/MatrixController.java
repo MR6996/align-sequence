1db07afebd7486b08e12b62e906673c11cfd2137
Mario Randazzo <Mario Randazzo@DESKTOP-B55GO8M.lan>
2018-11-07 23:35:27 +0100

    Fix matrix visualization

--- SequenceAlignment/src/main/java/application/MatrixController.java
+++ SequenceAlignment/src/main/java/application/MatrixController.java
@@ -2,9 +2,11 @@ package application;
 
 import algorithms.Cell;
 import javafx.fxml.FXML;
+import javafx.geometry.VPos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 
 public class MatrixController  {
 	
@@ -27,23 +29,30 @@ public class MatrixController  {
 	@FXML
     private Canvas matrixCanvas;
     
-    private GraphicsContext gfCtx;
-
     @FXML
     void initialize() {
+    	drawMatrix();
+    }
+    
+    private void drawMatrix() {
     	matrixCanvas.setWidth(SQUARE_SIZE*m);
     	matrixCanvas.setHeight(SQUARE_SIZE*n);
     	
-    	gfCtx = matrixCanvas.getGraphicsContext2D();
+    	GraphicsContext gfCtx = matrixCanvas.getGraphicsContext2D();
+    	gfCtx.setTextAlign(TextAlignment.CENTER);
+    	gfCtx.setTextBaseline(VPos.CENTER);
     	
     	for(int i = 0; i < n; i++)
     		for(int j = 0; j < m; j++) {
     			double value = 0.6*pMatrix[i][j].getScore()/maxScore + 0.4;
     			gfCtx.setFill(Color.hsb(196, 0.5, value));
-    			gfCtx.fillRect(SQUARE_SIZE*i, SQUARE_SIZE*j, SQUARE_SIZE, SQUARE_SIZE);
+    			gfCtx.fillRect(SQUARE_SIZE*j, SQUARE_SIZE*i, SQUARE_SIZE, SQUARE_SIZE);
+    			gfCtx.setFill(Color.BLACK);
+    			gfCtx.fillText(Float.toString(pMatrix[i][j].getScore()), SQUARE_SIZE*(j+0.5), SQUARE_SIZE*(i+0.5));
     		}
     	
-   }
+    }
+    
 
     
 }
